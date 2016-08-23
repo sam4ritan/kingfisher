872387837203c5b53817eece859dfd858b697ec4
sam4ritan <sam4ritan@eclipso.email>
2016-08-23 13:47:51 +0200

    First Window updated

--- main.java
+++ main.java
@@ -7,6 +7,7 @@ public class main
   	JFrame mainWindow = new JFrame("project kingfisher");
 	mainWindow.setSize(200,200);
 	mainWindow.setVisible(true);
+	mainWindow.add(new JLabel("Hier könnte IHRE Werbung stehen");
 	};
 };
 
