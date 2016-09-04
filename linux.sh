4cfd0b59f863dfa31eddac379b4ab7405e4879a9
sam4ritan <sam4ritan@eclipso.email>
2016-09-04 15:35:19 +0200

    cleanup after release branch

--- linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#'path' soll den derzeitigen Pfad auslesen und zur Verarbeitung ermöglichen, ausfälle durch Blanks sind einbezogen
-
-path="$PWD"
-
-java -jar "$path"/jar/kingfisher.jar #Startet Programm, Java wird vorausgesetzt
