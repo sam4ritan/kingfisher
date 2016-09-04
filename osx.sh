3406a09b10d406a62a8ff4ad550eabfde8d62035
sam4ritan <sam4ritan@eclipso.email>
2016-09-04 11:26:36 +0200

    added osx launcher script

---
+++ osx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#'path' soll den derzeitigen Pfad auslesen und zur Verarbeitung ermöglichen, ausfälle durch Blanks sind einbezogen
+
+#pathlin="$PWD"
+
+java -jar $pwd/jar/kingfisher_a0_0_3.jar #Startet Programm, Java wird vorausgesetzt
